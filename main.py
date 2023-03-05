3308d8a8ab2bcae9b67882bf76037c0dd604fc39
Tuman8 <i-a-l-e-x@yandex.ru>
2023-03-05 17:41:25 +0700

    Запуск парсинга не чаще 1 раза в сутки.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from time import sleep
-from functions import check_internet, get_content, insert_info_db, show_notify
-import schedule
+from functions import check_internet, get_content, insert_info_db, show_notify, get_last_time_access_site
+from datetime import datetime, timedelta
 
 
 check_url = 'https://www.google.com/'
@@ -16,10 +16,41 @@ def main():
     prices = get_content(donor_url)
     insert_info_db(prices)
     print('... сбор закончен ...')
-    sleep(5)
+
+
+def time_start_program():
+    # получаем время последнего обращения к сайту
+    last_access_site = get_last_time_access_site()
+    # узнаем когда нужно запускать скрипт в следующий раз (через сутки)
+    time_next_start = last_access_site + timedelta(days=1)
+    # получаем текущее время
+    now_time = datetime.now()
+
+    if now_time > time_next_start:
+        # print('время запустить программу')
+        return True
+    else:
+        # узнаем время ожидания
+        time_wait = time_next_start - now_time
+        # вычисляем время ожидания в секундах
+        how_sec = time_wait.total_seconds()
+        # встаем на паузу
+        return time_wait
 
 
 if __name__ == '__main__':
-    schedule.every().day.at('12:21').do(main)
     while True:
-        schedule.run_pending()
+        start_info = time_start_program()
+        if start_info is True:
+            main()
+        else:
+            # запускаем таймер обратного отсчета в терминале
+            _TIME = start_info.seconds
+            while _TIME > 0:
+                m, s = divmod(_TIME, 60)
+                h, m = divmod(m, 60)
+                print(f"\rДо следующего парсинга осталось: {int(h)}".rjust(3, '0'), f"{int(m)}".rjust(2, '0'),
+                      f"{s}".rjust(2, '0'), sep=':', end='')
+                _TIME -= 1
+                sleep(1)
+
